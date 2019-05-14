8ee6e85e5b7f5b22fb61037c7954557fa4b6bba0
Alex <belorukov.aaa@gmail.com>
2019-05-14 19:51:31 +0300

    Done

--- Dictionary/Dictionary/Dictionary.cpp
+++ Dictionary/Dictionary/Dictionary.cpp
@@ -1,21 +1,50 @@
-// Dictionary.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "pch.h"
 #include <iostream>
 
+using namespace std;
+
+class Dictionary
+{
+private:
+	int* _arr;
+	int _capacity;
+public:
+	Dictionary()
+	{
+		_arr = nullptr;
+		_capacity = 0;
+	}
+	~Dictionary()
+	{
+		delete[]_arr;
+	}
+	int & operator[](const char* key)
+	{
+		int index = 0;
+		for (int i = 0;i < strlen(key);i++)
+		{
+			index += key[i];
+		}
+		if (index > _capacity)
+		{
+			int* temp = new int[index+1] {};
+			for (int i = 0;i < _capacity;i++)
+			{
+				temp[i] = _arr[i];
+			}
+			delete[]_arr;
+			_arr = temp;
+			_capacity = index + 1;
+		}
+		return _arr[index];
+	}
+};
 int main()
 {
-    std::cout << "Hello World!\n"; 
+	Dictionary dict;
+	dict["test1"] = 1;
+	dict["key2"] = 33;
+	dict["qwerty"] = 5;
+	cout << dict["test1"] << " " << dict["key2"] << " " << dict["qwerty"] << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
